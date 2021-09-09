04ec794270f834598186b54e7f22b4ee124afaf2
LancePokaiWang <LanceWang@cmail.carleton.ca>
2021-09-09 12:35:21 +0800

    init rest api model

---
+++ go.mod
@@ -0,0 +1,7 @@
+module cienet.com/Golang_Web_Crawling
+
+go 1.17
+
+require (
+        github.com/gocolly/colly/v2 latest
+)
---
+++ rest_api/main.go
@@ -0,0 +1,10 @@
+package rest_api
+
+// product represents data of merchandise.
+type product struct {
+	ID     string
+	Name   string
+	price  int
+	rating float64
+	url    string
+}
