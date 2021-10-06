c44b1fe511b0c39bda782e4461e1fb4bf7a5ea7d
circlepen <b01505008@ntu.edu.tw>
2021-10-06 18:07:11 +0800

    add redis to docker-compose

--- redis/redis.go
+++ redis/redis.go
@@ -41,7 +41,7 @@ type Redis struct {
 
 func NewClient() Redis {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 	})
